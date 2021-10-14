78815de4fb8d94210b1ecd4cdab13b3723e1d7f8
Всеволод <63463168+vsecoder@users.noreply.github.com>
2021-10-14 19:15:14 +0300

    Add files via upload

---
+++ models/users.py
@@ -0,0 +1,15 @@
+import sqlalchemy as sa
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sa.Column(sa.Integer,
+                   primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String, nullable=True)
+    username = sa.Column(sa.String, nullable=True)
+    carma = sa.Column(sa.Integer, autoincrement=True)
+    bot = sa.Column(sa.String, nullable=True)
+    bad_words = sa.Column(sa.String, nullable=True)
+    words = sa.Column(sa.String, nullable=True)
